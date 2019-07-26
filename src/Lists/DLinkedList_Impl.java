241fcaaf0222d877f725024064ef87de70f91063
Viswasai-G <36977407+Viswasai-G@users.noreply.github.com>
2019-07-25 20:31:12 -0700

    Doubly linked list and changes to LL.

--- src/Lists/DLinkedList_Impl.java
+++ src/Lists/DLinkedList_Impl.java
@@ -2,7 +2,7 @@ package Lists;
 
 public class DLinkedList_Impl extends DNode {
 
-    private DNode head;
+    private DNode head = new DNode();
 
     public <T> DLinkedList_Impl(T data){
         head.setData(data);
@@ -14,11 +14,22 @@ public class DLinkedList_Impl extends DNode {
         DNode temp = head;
         while(temp.getNext()!=null){temp = temp.getNext();}
         DNode node = new DNode(data, null, temp);
+        temp.setNext(node);
     }
 
     public <T> void addhead(T data){
         DNode temp = head;
         head = new DNode(data, temp, null);
+    }
+
+    public void printList(){
+        DNode temp = head;
+        System.out.print("[");
+        while(temp.getNext()!= null){
+            System.out.print(temp.getData() + ",");
+            temp = temp.getNext();
+        }
+        System.out.print(temp.getData() + "]");
     }
 
     public DNode getHead() {
