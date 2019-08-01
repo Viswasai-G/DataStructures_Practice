0bd736d842fdd7500b0fc89775eedc45e7f0f37a
Viswasai-G <36977407+Viswasai-G@users.noreply.github.com>
2019-07-31 20:13:06 -0700

    Interface addition-Redo

---
+++ src/Lists/LL_interface.java
@@ -0,0 +1,9 @@
+package Lists;
+
+public interface LL_interface {
+    <T> void addHead(T data);
+    <T> void add(T data);
+    <T> void delete(T data);
+    void deleteHead();
+    void printList();
+}
