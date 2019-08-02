049306ffbdab6e89a20ec90a3fbc6492bdbfafc8
Viswasai-G <36977407+Viswasai-G@users.noreply.github.com>
2019-08-01 20:38:46 -0700

    Tree Node

---
+++ src/Lists/TreeNode.java
@@ -0,0 +1,46 @@
+package Lists;
+
+import java.util.ArrayList;
+
+public class TreeNode<T> {
+    private TreeNode parent;
+    private T data;
+    private ArrayList<TreeNode> children = null;
+
+    public TreeNode getParent() {
+        return parent;
+    }
+
+    public void setParent(TreeNode parent) {
+        this.parent = parent;
+    }
+
+    public TreeNode(T data, ArrayList<TreeNode> children) {
+        this.data = data;
+        this.children = children;
+    }
+
+    public TreeNode(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public ArrayList<TreeNode> getChildren() {
+        return children;
+    }
+
+    public void setChildren(ArrayList<TreeNode> children) {
+        this.children = children;
+    }
+
+    public void addChild(TreeNode child){
+        children.add(child);
+    }
+}
