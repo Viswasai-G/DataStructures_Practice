8ade91296ed5a96d39428632d6aecc1b9fd64d6c
Viswasai-G <36977407+Viswasai-G@users.noreply.github.com>
2019-08-05 20:39:28 -0700

    Tree Refactor

--- src/Trees/Tree_Impl.java
+++ src/Trees/Tree_Impl.java
@@ -1,11 +1,28 @@
 package Trees;
 
+import java.util.ArrayList;
+
+//Assuming there are no duplicates in the tree
 public class Tree_Impl<T> {
     private TreeNode<T> root;
 
     public Tree_Impl(T data){
         root.setData(data);
         root.setParent(null);
+    }
+
+    public void addrootchild(TreeNode child){
+        root.addChild(child);
+    }
+
+    public void deleterootChild(T data){
+        ArrayList<TreeNode> children = root.getChildren();
+        for(TreeNode child:children){
+            if(child.getData() == data){
+                children.remove(child);
+                break;
+            }
+        }
     }
 
 }
