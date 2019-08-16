eb8131915c029f7aa4154dd1b9210915207f173a
Viswasai-G <36977407+Viswasai-G@users.noreply.github.com>
2019-08-15 21:26:06 -0700

    Binary Tree method additions-pt1

--- src/Trees/BTree_Impl.java
+++ src/Trees/BTree_Impl.java
@@ -7,5 +7,34 @@ public class BTree_Impl<T> {
         this.root = new BinaryTreeNode(data);
     }
 
+    public void add(T data){
+        if(root.data == null){
+            root.data = data;
+        }
+        else{
+            BinaryTreeNode<T> temp = findEmpty(root);
+            if(temp.getLeft()==null){
+                temp.setLeft(new BinaryTreeNode(data, temp));
+            }
+            else temp.setRight(new BinaryTreeNode(data, temp));
+        }
+    }
 
+    public BinaryTreeNode findEmpty(BinaryTreeNode node){
+        if(hasBoth(node)){
+            BinaryTreeNode templeft = node.getLeft();
+            BinaryTreeNode tempright = node.getRight();
+            findEmpty(templeft);
+            findEmpty(tempright);
+            return null;
+        }
+        else return node;
+    }
+
+    public boolean hasBoth(BinaryTreeNode node){
+        if((node.getLeft()!=null) && (node.getRight()!=null)){
+            return true;
+        }
+        else return false;
+    }
 }
